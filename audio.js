6c5f439dcc281615f5493b237c2181a0964da877
SiwaSathya <110297807+SiwaSathya@users.noreply.github.com>
2022-09-14 19:54:47 +0800

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,20 +1,22 @@
-var musik=new Audio();
+ var musik=new Audio () ;
                         musik.src="wedding.mp3";
                         musik.loop=true;
+                        musik.autoplay=true;
                         musik.play();
 
                         function mulaiAudio(){
                             var play=document.getElementById("play");
                             play.addEventListener('click', fplay);
+                            musik.play();
 
                             function fplay(){
                                 if(musik.paused){
                                     musik.play();
-                                    play.getElementById("img").src = "pause.png";
+                                    play.style.background="url(pause.png)"
                                 }
                                 else{
                                     musik.pause();
-                                    play.getElementById("img").src = "play.png";
+                                    play.style.background="url(play.png)"
                                     
                                 }
                             }
