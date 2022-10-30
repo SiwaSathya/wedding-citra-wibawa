eac4e10ad231b0846b53b6adf05b8c8c68a7427b
SiwaSathya <siwasathya@gmail.com>
2022-10-30 21:26:22 +0800

    masang ke github

---
+++ audio.js
@@ -0,0 +1,24 @@
+                        var musik=new Audio () ;
+                        musik.src="wedding.mp3";
+                        musik.loop=true;
+                        musik.autoplay=true;
+                        musik.play();
+
+                        function mulaiAudio(){
+                            var play=document.getElementById("play");
+                            play.addEventListener('click', fplay);
+                            musik.play();
+
+                            function fplay(){
+                                if(musik.paused){
+                                    musik.play();
+                                    play.style.background="url(pause.png)"
+                                }
+                                else{
+                                    musik.pause();
+                                    play.style.background="url(play.png)"
+                                    
+                                }
+                            }
+                        }
+                        window.addEventListener('load', mulaiAudio);
